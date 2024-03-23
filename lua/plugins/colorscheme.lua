2a156802de341002bbd7abb3363d5ebf8bde6f0a
Jakub Owczarek <jakub.jan.owczarek@gmail.com>
2024-03-23 13:04:13 +0100

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  "catppuccin.nvim",
+}
+
+-- return {
+--   { "navarasu/onedark.nvim" },
+--   opts = {
+--     transparent = true,
+--   },
+-- }
+
+-- return {
+--   "tokyonight.nvim",
+--   opts = {
+--     transparent = true,
+--     styles = {
+--       sidebars = "transparent",
+--       floats = "transparent",
+--     },
+--   },
+-- }

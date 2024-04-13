72228d519276b21c6c346e735c88778f95aa02bb
Jakub Owczarek <jakub.jan.owczarek@gmail.com>
2024-04-13 12:18:27 +0200

    fix: change powershell to zsh

--- init.lua
+++ init.lua
@@ -1,22 +1,9 @@
-local powershell_options = {
-  shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  shellquote = "",
-  shellxquote = "",
-}
-
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
-end
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
 -- terminal emulator
-vim.g.terminal_emulator = "powershell"
-vim.opt.shell = "powershell.exe"
+vim.g.terminal_emulator = "zsh"
+vim.opt.shell = "zsh"
 
 -- neo vim font
 vim.opt.guifont = "Hack Nerd Font Mono:h12"
